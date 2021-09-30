a8e3d0958320f268537df4e72bf093b26e2228ea
Jack Ault <henlowgg@outlook.com>
2021-09-30 16:24:54 -0400

    starting the wet prompts via inquirer, will dry it up later

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// inquirer to generate questions
+inquirer.prompt(
+    [
+        {
+            type: 'input',
+            message="What is the project title?",
+            name:'title',
+            validate: (value)=>{ if (value){return true} else {return 'Please enter a value to continue'}},
+        },
+        {
+            type: 'input',
+            message: "How is the application installed?",
+            name: 'installation',
+            validate: (value)=>{ if (value){return true} else {return 'Please enter a value to continue'}},
+        }
+    ]
+)
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
